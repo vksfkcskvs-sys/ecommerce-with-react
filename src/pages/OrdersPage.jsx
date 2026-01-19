d6138ea16b11f5b5cb3cd64bdac8ae7e8bbcb458
Kunal <vksfkcskvs@gmail.com>
2026-01-19 16:55:45 +0530

    split header into a component

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,10 +1,12 @@
-import './header.css';
+import { Header } from '../components/header';
 import './OrdersPage.css';
 
 export function OrdersPage() {
     return (
         <>
             <title>Orders</title>
+
+            <Header />
 
             <div className="header">
                 <div className="left-section">
