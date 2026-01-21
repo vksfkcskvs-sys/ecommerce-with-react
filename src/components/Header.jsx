1ba7ba9578c8a0e2cca1603c7f3ce135d2a460b7
Kunal <vksfkcskvs@gmail.com>
2026-01-21 16:53:47 +0530

    did some exercises and made the search input on the header more interactive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
-import { Link } from 'react-router';
+import { useState } from 'react';
+import { Link, useNavigate, useSearchParams } from 'react-router';
 import './header.css';
 
 export function Header({ cart }) {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const searchText = searchParams.get('search');
+  const [search, setSearch] = useState(searchText || '');
+
+  const updateSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+  const searchProducts = () => {
+    navigate(`/?search=${search}`);
+  };
   let totalQuantity = 0;
   cart.forEach((cartItem) => {
     totalQuantity += cartItem.quantity;
   });
-
-
   return (
     <div className="header">
       <div className="left-section">
@@ -18,21 +28,18 @@ export function Header({ cart }) {
             src="images/mobile-logo-white.png" />
         </Link>
       </div>
-
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
-
-        <button className="search-button">
+        <input className="search-bar" type="text" placeholder="Search"
+          value={search} onChange={updateSearchInput} />
+        <button className="search-button"
+          onClick={searchProducts}>
           <img className="search-icon" src="images/icons/search-icon.png" />
         </button>
       </div>
-
       <div className="right-section">
         <Link className="orders-link header-link" to="/orders">
-
           <span className="orders-text">Orders</span>
         </Link>
-
         <Link className="cart-link header-link" to="/checkout">
           <img className="cart-icon" src="images/icons/cart-icon.png" />
           <div className="cart-quantity">{totalQuantity}</div>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,31 +1,31 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router';
 import { Header } from '../../components/Header';
-import './HomePage.css';
 import { ProductsGrid } from './ProductsGrid';
+import './HomePage.css';
 
-
-export function HomePage({cart, loadCart}) {
+export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('search');
 
   useEffect(() => {
     const getHomeData = async () => {
-      const response = await axios.get('/api/products')
-      setProducts(response.data); 
-    }; 
+      const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+      const response = await axios.get(urlPath);
+      setProducts(response.data);
+    };
 
     getHomeData();
-  }, []);
-  
+  }, [search]);
 
   return (
     <>
       <title>Ecommerce Project</title>
-
-      <Header cart={cart}/>
-
+      <Header cart={cart} />
       <div className="home-page">
-        <ProductsGrid products={products} loadCart={loadCart}/>
+        <ProductsGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
